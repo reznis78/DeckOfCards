9db92be930b8c3710b889264f516f2726e138279
Nathan Uhl <nattyu@hotmail.com>
2017-10-26 16:28:47 +0200

    Add project files.

---
+++ DeckOfCards.Test/DeckUnitTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeckOfCards.Test
+{
+    [TestClass]
+    public class DeckUnitTests
+    {
+        [TestMethod]
+        public void SizeOfDeck()
+        {
+            Deck testDeck = new Deck();
+
+            Assert.AreEqual(testDeck.Count, 52);
+        }
+
+        [TestMethod]
+        public void NoDuplicates()
+        {
+            Deck testDeck = new Deck();
+
+            foreach (var item in testDeck)
+            {
+
+            }
+        }
+    }
+}
